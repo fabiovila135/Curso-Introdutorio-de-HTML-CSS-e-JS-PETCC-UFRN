7eaafd34b32b8e637c695a3b7c2406bd263f1c1d
FabioVila <fabio.vila.135@ufrn.edu.br>
2024-02-20 09:36:15 -0300

    dia 1 + dia 2 js

---
+++ petcc/media.js
@@ -0,0 +1,5 @@
+let numero1 = Number(prompt("Insira o primeiro número: "));
+let numero2 = Number(prompt("Insira o segundo número: "));
+let numero3 = Number(prompt("Insira o terceiro número: "));
+
+console.log("A média entre " + numero1 + ", " + numero2 + " e " + numero3 + " é " + ((numero1 + numero2 + numero3)/3));
