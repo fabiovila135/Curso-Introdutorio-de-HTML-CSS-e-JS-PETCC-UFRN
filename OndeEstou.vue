26111b9ad9d6b538391ad2c171d1021eb3cc3473
fabiovila135 <113956183+fabiovila135@users.noreply.github.com>
2024-02-21 11:55:03 -0300

    dia 3 exercicios

---
+++ OndeEstou.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="container"> 
+        <div id="pergunta">
+            <h1> Em qual prédio estamos? </h1>
+        </div>
+        <div id="prompt">
+
+        </div>
+        <div id="resposta">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
