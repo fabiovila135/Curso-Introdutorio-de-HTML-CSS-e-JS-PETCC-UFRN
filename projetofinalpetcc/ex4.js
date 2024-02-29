3bb6ba27060b8f0432b0daf48ab5821cbfaed38c
fabiovila135 <fabio.vila.135@ufrn.edu.br>
2024-02-29 00:57:05 -0300

    muitas mudanças, sinceramente nao sei oq fazer +

--- projetofinalpetcc/ex4.js
+++ projetofinalpetcc/ex4.js
@@ -1,3 +1,21 @@
-function myFunction(resposta) {
-    document.getElementById("answer").innerHTML = document.getElementById("resposta").innerHTML;
+let respostaCorreta= 'Debut';
+
+function selecionarAlternativa(alternativaSelecionada) { 
+    let alternativas = document.querySelectorAll('.alternativa_filha');
+    for (let i = 0; i < alternativas.length; ++i) {
+        alternativas[i].classList.remove('destaque');
+    }
+    alternativaSelecionada.classList.add('destaque');
+
+    var texto = document.getElementsByClassName('destaque');
+    resp.innerHTML = texto;
+}
+
+
+function comparar() {
+    var texto = document.getElementsByClassName('destaque')
+    
+    if(texto === respostaCorreta) {
+        resp.innerHTML = "Correto!"
+    }
 }
